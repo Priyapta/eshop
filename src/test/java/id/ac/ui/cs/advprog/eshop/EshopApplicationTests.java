07e39c67c07aaa9b6b7f78c371cff108bd9da735
Priyapta Naufal <priaptanaufal@gmail.com>
2025-02-18 15:28:21 +0700

    Revert "modified unit-test"

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -2,22 +2,12 @@ package id.ac.ui.cs.advprog.eshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
-class EshopApplicationTest {
+class EshopApplicationTests {
 
     @Test
-    void contextLoads(ApplicationContext context) {
-        // Memastikan bahwa konteks Spring Boot berhasil dimuat
-        assertNotNull(context, "Konteks Spring Boot harus berhasil dimuat.");
+    void contextLoads() {
     }
 
-    @Test
-    void mainMethodRunsSuccessfully() {
-        // Memastikan bahwa metode main dapat dijalankan tanpa exception
-        EshopApplication.main(new String[]{""});
-    }
 }
